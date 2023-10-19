20bddf962a2a985670e848c3267b585cd57417f4
d3nnyyy <d3nnn41k@gmail.com>
2023-10-19 16:38:43 +0300

    created model class

--- REST/src/main/java/ua/dtsebulia/rest/model/ShoeModel.java
+++ REST/src/main/java/ua/dtsebulia/rest/model/ShoeModel.java
@@ -1,4 +1,28 @@
 package ua.dtsebulia.rest.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
 public class ShoeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private Integer price;
+    private Integer size;
+    private String color;
+
 }
